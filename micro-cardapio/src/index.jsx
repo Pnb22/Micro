94ac9ea339b8561cf48db12a54de23e793f41a63
arthur fonseca <arthurfgp22@gmail.com>
2026-05-20 22:04:25 -0300

    0

--- micro-cardapio/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import CardapioApp from './CardapioApp';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<CardapioApp />);
--- micro-pedido/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PedidoApp from './PedidoApp';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<PedidoApp />);
